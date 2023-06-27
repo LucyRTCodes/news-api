4fe0e21f0cd67ef418f5e93449e8cc010467ab1d
LucyRTCodes <lucyrtutton@gmail.com>
2023-06-27 11:46:36 +0100

    Apparently I have changes in my model and app

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 const express = require("express");
-const { getAllTopics,
-	getAllArticles, getArticleById } = require("./controllers/app.controller");
+const {
+	getAllTopics,
+	getAllArticles,
+	getArticleById,
+} = require("./controllers/app.controller");
 
 const app = express();
 
 app.get("/api/topics", getAllTopics);
 app.get("/api/articles", getAllArticles);
-
-app.use((err, req, res, next) => {
-	if (err.code === "22P02") {
-		res.status(400).send({ msg: "Bad request" });
-	} else if (err.msg && err.status) {
-		res.status(err.status).send({ msg: err.msg });
-	} else {
-		res.status(500).send({ msg: "Internal server error" });
-	}
-});
 app.get("/api/articles/:article_id", getArticleById);
 
 app.use((err, req, res, next) => {
